645f4effaa24a8d22cdf879083e1b6d708c4bef7
copeelite <chef777777777@gmail.com>
2022-08-25 22:06:41 -0400

    Updates

---
+++ precache-manifest.950b7f704c168d83dd0c1d4f7268c105.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1ef4f8e37cb031db320",
+    "url": "/carvel/static/css/main.5ca7c8f5.chunk.css"
+  },
+  {
+    "revision": "b1ef4f8e37cb031db320",
+    "url": "/carvel/static/js/main.b1ef4f8e.chunk.js"
+  },
+  {
+    "revision": "b3be8a9b1cc833812d75",
+    "url": "/carvel/static/js/1.b3be8a9b.chunk.js"
+  },
+  {
+    "revision": "7ee2085193e4bf4d45d3",
+    "url": "/carvel/static/js/2.7ee20851.chunk.js"
+  },
+  {
+    "revision": "2f6e06a8e28e098116e5",
+    "url": "/carvel/static/js/runtime~main.2f6e06a8.js"
+  },
+  {
+    "revision": "229fc51fb4aab859bad128fb17f8d696",
+    "url": "/carvel/index.html"
+  }
+];
